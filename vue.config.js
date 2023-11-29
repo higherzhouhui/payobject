cdc07d18d5479b1a25557594a25f1bbc4aee7b05
hc <zhouhui608@gmail.com>
2023-11-29 14:00:45 +0800

    优化打包

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 const path = require('path');
-
+const Timestamp = new Date().getTime();
 module.exports = defineConfig({
   publicPath: './',
   assetsDir: 'static',
@@ -26,21 +26,28 @@ module.exports = defineConfig({
   },
   lintOnSave: false,
   chainWebpack: config => {
-   // set svg-sprite-loader
-   config.module
-   .rule('svg')
-   .exclude.add(path.resolve(__dirname, 'src/components/svgIcon/svg'))
-   .end()
-   config.module
-   .rule('icons')
-   .test(/\.svg$/)
-   .include.add(path.resolve(__dirname, 'src/components/svgIcon/svg'))
-   .end()
-   .use('svg-sprite-loader')
-   .loader('svg-sprite-loader')
-   .options({
-       symbolId: 'icon-[name]'
-   })
-   .end()
+    // set svg-sprite-loader
+    config.module
+      .rule('svg')
+      .exclude.add(path.resolve(__dirname, 'src/components/svgIcon/svg'))
+      .end()
+    config.module
+      .rule('icons')
+      .test(/\.svg$/)
+      .include.add(path.resolve(__dirname, 'src/components/svgIcon/svg'))
+      .end()
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
+      .options({
+        symbolId: 'icon-[name]'
+      })
+      .end()
+  },
+
+  configureWebpack: {
+    output: {// 输出重构 打包编译后的文件名称 【输出文件夹/文件名(name为默认文件名非参数).时间戳】
+      filename: `static/js/[name].${Timestamp}.js`,
+      chunkFilename: `static/js/[name].${Timestamp}.js`
+    },
   },
 })
